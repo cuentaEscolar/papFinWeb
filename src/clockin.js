8f3b0f495118336d8140e00ecafdfe42d00e71ee
Regalado <lregaladofloriano@gmail.com>
2026-07-10 14:19:00 -0600

    fully working clockin 	modified:   src/clockin.js

--- src/clockin.js
+++ src/clockin.js
@@ -1,2 +1,35 @@
-const jwt = require('jsonwebtoken');
-const authenticateToken = require('../middleware/auth');
+
+document.getElementById('clockin-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const empleado = document.getElementById('employee').value;
+  const hora = document.getElementById('entryTime').value;
+  console.log(empleado, hora);
+  try {
+
+    const res = await fetch('/api/clockin', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ empleado, hora })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert('Registro Exitoso ' + empleado);
+      // Redirigir o mostrar perfil
+      console.log("ieeeeei. peace peace")
+      //const modal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
+      //modal.hide();
+    } else {
+      alert(data.error);
+    }
+
+  } catch (err) {
+    console.error(err);
+    alert('Error en la conexión');
+  }
+
+});
+
