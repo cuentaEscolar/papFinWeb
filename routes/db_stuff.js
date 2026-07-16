9a04fdebd6c0d624f5f4ccd7e1f8116f5dc9d881
Regalado <lregaladofloriano@gmail.com>
2026-07-16 00:37:20 -0600

    everything working 	new file:   BetterClockin.html 	new file:   RegisterSalariado.html 	modified:   RegisterUser.html 	modified:   routes/db_stuff.js 	new file:   src/better_clockin.js 	new file:   vendor/htmx/htmx.min.js

--- routes/db_stuff.js
+++ routes/db_stuff.js
@@ -5,6 +5,17 @@ const pool = require('../config/db');
 
 
 
+router.get('/api/employee/names', async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT id, nombre from NOMBRES;");
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 router.get('/api/weekly_payroll/total', async (req, res) => {
   try {
     const [rows] = await pool.query("call get_last_week_payroll_total();");
@@ -64,6 +75,25 @@ router.post('/api/clockout', async (req, res) => {
 
 
 
+router.post('/api/clockin/id', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { empleado, hora } = req.body;
+
+    // Buscar usuario
+    await pool.query('call clockin_id(?, ?)', [empleado, hora]);
+
+    res.status(200).json({
+      success: true,
+      message: 'Entrada registrada'
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error en clockin' });
+  }
+});
+
+
 router.post('/api/clockin', async (req, res) => {
   try {
     console.log(req.body);
@@ -115,47 +145,12 @@ router.post('/api/register_regular_user', async (req, res) => {
     if (user.length === 0) {
       return res.status(401).json(
         {
-          error: 'Usuario no encontrado'
+          error: 'Usuario no creado'
         }
       );
     }
-    const user_id = user[0].id;
-    console.log("user id");
-    console.log(user_id);
-    console.log(user_id);
-    const [rows] = await pool.query('SELECT * FROM PASSWORDS_CAPTURISTAS WHERE id = ?', [user_id]);
-    console.log('response', rows);
-    const user_p = rows[0].password;
-    console.log(rows);
 
-    console.log(password, user_p)
-    // Comparar contraseña
-    const validPassword = await bcrypt.compare(password, user_p);
-    if (!validPassword) {
-      return res.status(401).json({ error: 'Contraseña incorrecta' });
-    }
-
-    // Crear token JWT
-    const token = jwt.sign(
-      {
-        id: user.idUsuario,
-        name: user.nombre,
-        email: user.email,
-        phone: user.telefono,
-        isprovider: user.rol
-      },
-      process.env.JWT_SECRET || 'mi_clave_secreta',
-      { expiresIn: '1h' }
-    );
-
-    // Guardar token en cookie HTTP-only
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: false,
-      maxAge: 3600000 // 1 hora
-    });
-
-    res.json({ message: 'Login exitoso', user: { id: user.id, name: user.name, email: user.email } });
+    res.json({ message: 'Login exitoso', user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error en login' });
---
+++ src/better_clockin.js
@@ -0,0 +1,63 @@
+
+import { is_admin } from "./force_login.js";
+
+async function loadUsers() {
+
+  console.log("loading users");
+  const response = await fetch('/api/employee/names');
+  const maquilas = await response.json();
+  console.log(maquilas);
+
+  const select = document.getElementById("id_empleado");
+
+  select.innerHTML = "";
+
+  maquilas.forEach(empleado => {
+
+    const option = document.createElement("option");
+    option.value = empleado.id;
+    option.textContent = empleado.nombre;
+
+    select.appendChild(option);
+
+  });
+
+}
+is_admin();
+console.log("about to load users")
+loadUsers();
+
+document.getElementById('clockin-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const empleado = document.getElementById('id_empleado').value;
+  const hora = document.getElementById('entryTime').value;
+  console.log(empleado, hora);
+  try {
+
+    const res = await fetch('/api/clockin/id', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ empleado, hora })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert('Registro Exitoso ' + empleado);
+      // Redirigir o mostrar perfil
+      console.log("ieeeeei. peace peace")
+      //const modal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
+      //modal.hide();
+    } else {
+      alert(data.error);
+    }
+
+  } catch (err) {
+    console.error(err);
+    alert('Error en la conexión');
+  }
+
+});
+
